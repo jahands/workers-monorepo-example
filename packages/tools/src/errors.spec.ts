e857ba78e77c133a2a221741de45f16ebce6eb46
Jacob Hands <jacob@gogit.io>
2024-09-17 04:45:09 -0500

    Remove unused imports

--- packages/tools/src/errors.spec.ts
+++ packages/tools/src/errors.spec.ts
@@ -1,7 +1,6 @@
 import { Command, program } from '@commander-js/extra-typings'
 import { beforeAll, beforeEach, describe, expect, it } from 'vitest'
 
-import { getConfig } from './config'
 import { cliError } from './errors'
 
 const exitErrors: Error[] = []
